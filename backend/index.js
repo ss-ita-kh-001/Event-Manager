da7cfb668cb98edf303857ec93487b7336cf5a42
maryna.yelakova <maryna.yelakova@gmail.com>
2016-09-07 16:20:50 +0300

    Add core folders, nodejs web server and index.html dummy

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../frontend'));
+
+app.listen(process.env.PORT || 3000,function () {
+    console.log('Example app listening on port 3000!');
+});
+
