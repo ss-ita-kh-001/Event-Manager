6dcd8ea80586d22ab41d32bcc7049a4380e06db5
Anton Myshenin <myshenin.contact@gmail.com>
2016-09-11 21:44:07 +0300

    layout with bootstrap
    small bootstrap build is used (typography, grid system, responsive
    utilities)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+
 gulp.task("default", ["sass:watch"]);
+
 gulp.task("sass", function () {
-  return gulp.src('frontend/core/layout/scss/layout.scss')
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('frontend/core/layout/css'));
+  return gulp.src('frontend/core/layout-bootstrap/scss/layout.scss')
+   .pipe(sass({outputStyle:"compressed"}).on('error', sass.logError))
+   .pipe(rename(function (path){
+     path.basename+=".min";
+   }))
+   .pipe(gulp.dest('frontend/core/layout-bootstrap/css/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('frontend/core/layout/scss/*.scss', ['sass']);
+  gulp.watch('frontend/core/layout-bootstrap/scss/*.scss', ['sass']);
 });
