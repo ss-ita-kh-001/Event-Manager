f677dd527726e0cdc55553ab1c36855b2a478a68
Anton Myshenin <myshenin.contact@gmail.com>
2016-09-13 20:35:05 +0300

    maybe the last version of layout

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var rename = require("gulp-rename");
 gulp.task("default", ["sass:watch"]);
 
 gulp.task("sass", function () {
-  return gulp.src('frontend/core/layout-bootstrap/scss/layout.scss')
+  return gulp.src('frontend/core/layout/scss/layout.scss')
    .pipe(sass({outputStyle:"compressed"}).on('error', sass.logError))
    .pipe(rename(function (path){
      path.basename+=".min";
    }))
-   .pipe(gulp.dest('frontend/core/layout-bootstrap/css/'));
+   .pipe(gulp.dest('frontend/core/layout/css/'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('frontend/core/layout-bootstrap/scss/*.scss', ['sass']);
+  gulp.watch('frontend/core/layout/scss/*.scss', ['sass']);
 });
