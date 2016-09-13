bcb1636f6475d0f243351030e4d40856676dc723
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-09-13 17:10:43 +0300

    add user.css, routing to user page

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -6,6 +6,9 @@
                 templateUrl: "../features/main/views/main.html",
                 controller: "em.main.mainController"
             })
+            .when("/user", {
+                templateUrl: "../features/user/views/index.html"
+            })
             .otherwise({
                 template: "../features/main/views/main.html"
             });
