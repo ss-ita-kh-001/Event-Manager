2d88a3191e30d35a2797f82ecfe7e6f46410bb21
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-09-14 10:19:42 +0300

    change html5Mode  true

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,6 +1,6 @@
 (function() {
     angular.module("em").config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(false);
+        $locationProvider.html5Mode(true);
         $routeProvider
             .when("/", {
                 templateUrl: "../features/main/views/main.html",
