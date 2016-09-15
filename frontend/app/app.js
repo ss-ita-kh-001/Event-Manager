60f21e78a51b3ac3275de6c35f951c1370616500
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-09-15 22:28:22 +0300

    fix slider

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,26 +1,61 @@
 (function() {
-    var app = angular.module("em", ["ngRoute", "em.main", 'ngAnimate', "ngSanitize" , "ui.bootstrap", "satellizer"]);
-    app.controller('DropdownCtrl', function($scope, $log) {
-        $scope.items = [
-            'The first choice!',
-            'And another choice for you.',
-            'but wait! A third!'
-        ];
+    var app = angular.module("em", ["ngRoute", "em.main", 'ngAnimate', "ngSanitize", "ui.bootstrap", "satellizer"]);
+    
+    app.controller('CarouselDemoCtrl', function($scope) {
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
 
-        $scope.status = {
-            isopen: false
+        $scope.addSlide = function() {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: '//unsplash.it/' + newWidth + '/300',
+                text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+                id: currIndex++
+            });
         };
 
-        $scope.toggled = function(open) {
-            $log.log('Dropdown is now: ', open);
+        $scope.randomize = function() {
+            var indexes = generateIndexesArray();
+            assignNewIndexesToSlides(indexes);
         };
 
-        $scope.toggleDropdown = function($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            $scope.status.isopen = !$scope.status.isopen;
-        };
+        for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+        }
 
-        $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+        // Randomize logic below
+
+        function assignNewIndexesToSlides(indexes) {
+            for (var i = 0, l = slides.length; i < l; i++) {
+                slides[i].id = indexes.pop();
+            }
+        }
+
+        function generateIndexesArray() {
+            var indexes = [];
+            for (var i = 0; i < currIndex; ++i) {
+                indexes[i] = i;
+            }
+            return shuffle(indexes);
+        }
+
+        // http://stackoverflow.com/questions/962802#962890
+        function shuffle(array) {
+            var tmp, current, top = array.length;
+
+            if (top) {
+                while (--top) {
+                    current = Math.floor(Math.random() * (top + 1));
+                    tmp = array[current];
+                    array[current] = array[top];
+                    array[top] = tmp;
+                }
+            }
+
+            return array;
+        }
     });
 })();
