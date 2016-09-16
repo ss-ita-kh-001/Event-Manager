7d1d9d4b02c25d3c19cd1259be98cd7e2ca4c15a
Anton Myshenin <myshenin.contact@gmail.com>
2016-09-16 06:48:08 +0300

    still crashes

--- frontend/app/config/config.js
+++ frontend/app/config/config.js
@@ -18,6 +18,9 @@
             .when("/register", {
                 templateUrl: "./app/features/register/views/register.html"
             })
+            .when("/event/add", {
+                  templateUrl: "./app/features/addEvent/views/addEvent.html"
+            })
             .otherwise({
                 template: "./app/features/main/views/main.html"
             });
---
+++ frontend/app/features/addEvent/controllers/addEventController.js
@@ -0,0 +1,6 @@
+(function () {
+  angular.module("em.addEvent").controller("em.addEvent.addEventController", addEventController);
+  function addEventController($scope) {
+
+  }
+})();
