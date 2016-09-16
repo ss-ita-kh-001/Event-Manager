39811dfe5b86972277f3b86bb2a1d1cead336260
maryna.yelakova <maryna.yelakova@gmail.com>
2016-09-16 21:00:37 +0300

    change test.js

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btnUpdateTime").onclick = showTime;
-    showTime();
-});
-
-function showTime(){
-    var now = new Date();
-    document.getElementById("pTime").innerHTML = now;
-}
 function getSum() {
     return 2 + 2;
 }
