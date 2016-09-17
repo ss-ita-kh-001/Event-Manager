002c1a3b80f3ae9a23271467177b0fe8c3f0ba6b
Anna Slichenko <anslichenko@gmail.com>
2016-09-17 19:48:52 +0300

    add alignment

--- frontend/app/features/addEvent/directives/addEventDirective.js
+++ frontend/app/features/addEvent/directives/addEventDirective.js
@@ -71,24 +71,24 @@
                 }
             };
         })
-        .directive("fileread", [function () {
-    return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                var reader = new FileReader();
-                reader.onload = function (loadEvent) {
-                    scope.$apply(function () {
-                        scope.fileread = loadEvent.target.result;
+        .directive("fileread", [function() {
+            return {
+                scope: {
+                    fileread: "="
+                },
+                link: function(scope, element, attributes) {
+                    element.bind("change", function(changeEvent) {
+                        var reader = new FileReader();
+                        reader.onload = function(loadEvent) {
+                            scope.$apply(function() {
+                                scope.fileread = loadEvent.target.result;
+                            });
+                        }
+                        reader.readAsDataURL(changeEvent.target.files[0]);
                     });
                 }
-                reader.readAsDataURL(changeEvent.target.files[0]);
-            });
-        }
-    }
-}]);
+            }
+        }]);
 
 
 })();
