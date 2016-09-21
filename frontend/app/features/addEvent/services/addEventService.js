01a0ecdf280e0516a1ed13e879db17680bb724e8
Anton Myshenin <myshenin.contact@gmail.com>
2016-09-21 10:02:59 +0300

    add event become service and fixed problems with id

---
+++ frontend/app/features/addEvent/services/addEventService.js
@@ -0,0 +1,24 @@
+(function() {
+    angular.module("em.addEvent").service("em.addEvent.addEventService", addEventService);
+
+    function addEventService($scope, datePicker, map) {
+        this.add = function() {
+            var addedEvent = {};
+            addedEvent.title = $scope.title;
+            addedEvent.description = $scope.description;
+            addedEvent.date = $scope.datePicker.dt;
+            addedEvent.place = $scope.place;
+            addedEvent.uploadme = $scope.uploadme;
+
+            var events = JSON.parse(localStorage.getItem("events"));
+            if (events === null) {
+                events = [];
+            }
+
+            addedEvent.id = events.length;
+            events[events.length] = addedEvent;
+            localStorage.setItem("events", JSON.stringify(events));
+        }
+    }
+    addEventService.$inject = ["$scope", "em.addEvent.datePicker", "em.addEvent.map"];
+})();
