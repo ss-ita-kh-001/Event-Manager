50180075f71b8d5aa38a03c459f02e6107fc1909
DariaKanshina <daria.kanshyna@gmail.com>
2016-09-21 23:56:24 +0300

    add exception on event page

--- frontend/app/features/events/controllers/eventController.js
+++ frontend/app/features/events/controllers/eventController.js
@@ -2,22 +2,30 @@
     angular.module("em.events").controller("em.events.eventController", eventController);
 
     function eventController($scope, $routeParams, map, localStorageService) {
+
+
           $scope.path = $routeParams.id;
           $scope.events = JSON.parse(localStorageService.get('events'))
+          $scope.eventNotFound = false;
 
           angular.forEach($scope.events, function(event,  path ){
            if(event.id == $scope.path){
              $scope.event = event;
            }
          });
-          console.log($scope.event, 'vw');
-          console.log($scope.events, 'local');
 
-          var lat = $scope.event.place.lat;
-          var lng = $scope.event.place.lng;
 
-          map.init();
-          map.staticMarker(lat, lng);
+         if(!$scope.event){
+           $scope.eventNotFound = true;
+         }else{
+            var lat = $scope.event.place.lat;
+            var lng = $scope.event.place.lng;
+            map.init();
+            map.staticMarker(lat, lng);
+         }
+
+
+
           //
           // angular.forEach($scope.events, function(events,  path ){
           //   if(events.id == $scope.path){
