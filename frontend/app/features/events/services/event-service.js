5d7a14e7d9d1b4592a8a017de58c563b6b374b08
ponata <pobernatali@gmail.com>
2016-09-23 01:20:59 +0300

    add filters to events table, fix responsive styles to main view

--- frontend/app/features/events/services/event-service.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    angular.module("em.events").service("em.events.event-service", eventService);
-    function eventService(mockedEventList) {
-        this.getEvents = function() {
-            return mockedEventList;
-        }
-    }
-    eventService.$inject = ["em.events.mock-event-list"]
-})();
