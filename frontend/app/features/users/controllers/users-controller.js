da74d06aa61cf8fa82b1ec347039803e3f98c26e
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-09-30 13:48:29 +0300

    fix issues #71 #72

--- frontend/app/features/users/controllers/users-controller.js
+++ frontend/app/features/users/controllers/users-controller.js
@@ -2,7 +2,7 @@
     angular.module("em.users").controller("em.users.users-controller", usersController);
 
     function usersController($scope, usersService) {
-        $scope.users = usersService.getUsers();
+    	$scope.users = usersService.getUsers();        
     }
     usersController.$inject = ["$scope", "em.users.users-service"];
 })();
