b88ba79595257e9196fbf99db1589b4166e5ce0c
Anna Slichenko <anslichenko@gmail.com>
2016-10-02 10:14:08 +0300

    remove excessive logs

--- frontend/app/features/chat/directives/enterSenderDirective.js
+++ frontend/app/features/chat/directives/enterSenderDirective.js
@@ -1,8 +1,6 @@
 (function() {
     angular.module('em.chat')
     .directive('enterClicker', function enterClicker() {
-      console.log('dir');
-        console.log('didsfsdfr');
         return function(scope, element, attrs) {
             element.bind("keydown keypress", function(event) {
                 if (event.which === 13) {
