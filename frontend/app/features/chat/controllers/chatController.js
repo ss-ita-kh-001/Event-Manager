74d8bd0dac63b1c4e27e0cf07d3ce08dc43cc80d
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-03 16:50:24 +0300

    add short login form

--- frontend/app/features/chat/controllers/chatController.js
+++ frontend/app/features/chat/controllers/chatController.js
@@ -1,24 +1,37 @@
-
 (function() {
     angular.module("em.chat").controller("em.chat.chatController", chatController);
 
     function chatController($scope, chatService) {
+        // console.log("$scope.login = " + $scope.login);
+        if (sessionStorage.getItem("chatLogin")) {
+            $scope.login = true;
+        }
+        else {
+          $scope.login = false;
+        }
+
+        $scope.chatLogin = function () {
+          var login = document.forms.login.login.value;
+
+          sessionStorage.setItem("chatLogin", login);
+          $scope.login = true;
+          $scope.username = login;
+        }
 
         $scope.msgSend = function() {
             var msg = document.forms.publish.message.value;
-            if (msg !== ''){
-              chatService.msgSend(msg);
-              document.forms.publish.message.value = '';
-              var objDiv = document.querySelector('.chat-body');
-              objDiv.scrollTop = objDiv.scrollHeight;
+            if (msg !== '') {
+                chatService.msgSend(msg);
+                document.forms.publish.message.value = '';
+                var objDiv = document.querySelector('.chat-body');
+                objDiv.scrollTop = objDiv.scrollHeight;
 
-              console.log(objDiv.scrollTop);
-              console.log(objDiv.scrollHeight);
+                console.log(objDiv.scrollTop);
+                console.log(objDiv.scrollHeight);
 
             } else {
-              alert('Enter something!');
+                alert('Enter something!');
             }
-
 
         };
         // history
