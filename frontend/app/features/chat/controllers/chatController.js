fc60b99f64ff50eb6bafbe416fa81d8827495c80
Anna Slichenko <anslichenko@gmail.com>
2016-10-03 16:02:31 +0300

    fix new message display

--- frontend/app/features/chat/controllers/chatController.js
+++ frontend/app/features/chat/controllers/chatController.js
@@ -2,31 +2,31 @@
 (function() {
     angular.module("em.chat").controller("em.chat.chatController", chatController);
 
-    function chatController($scope, chatService) {
-
+    function chatController($scope, chatService, $timeout) {
+       var self = this;
+        self.scrollBottom = function() {
+          var objDiv = document.querySelector('.chat-body');
+          objDiv.scrollTop = objDiv.scrollHeight;
+        };
         $scope.msgSend = function() {
             var msg = document.forms.publish.message.value;
             if (msg !== ''){
               chatService.msgSend(msg);
               document.forms.publish.message.value = '';
-              var objDiv = document.querySelector('.chat-body');
-              objDiv.scrollTop = objDiv.scrollHeight;
-
-              console.log(objDiv.scrollTop);
-              console.log(objDiv.scrollHeight);
-
+              $timeout(self.scrollBottom, 10);
             } else {
               alert('Enter something!');
             }
-
-
         };
         // history
         $scope.history = chatService.history;
         // new messages
         $scope.live = chatService.live;
+
+        $timeout(self.scrollBottom, 1000);
+
     }
 
-    chatController.$inject = ["$scope", "em.chat.chatService"];
+    chatController.$inject = ["$scope", "em.chat.chatService", "$timeout"];
 
 })();
