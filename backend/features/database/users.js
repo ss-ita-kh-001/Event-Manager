6de3870443ecdc5a70812d18fa84065e4940d262
maryna.yelakova <maryna.yelakova@gmail.com>
2016-10-03 17:49:58 +0300

    change addUser and updateUser functions

--- backend/features/database/users.js
+++ backend/features/database/users.js
@@ -8,10 +8,15 @@
              return db.query("SELECT FROM \"users\" WHERE \"id\" = " + id + ";");
         },
         this.addUser = function(user) {
-            return db.query("INSERT INTO \"users\" (full_name,login,password,email,status) VALUES('" + user.fullName + "', '" + user.login + "','" + user.password + "','" + user.email + "','" + user.status + "');");
+            return db.query("INSERT INTO \"users\"(\"full_name\", \"login\", \"password\", \"email\", \"status\")" +
+                " VALUES(\'" + user.fullName + "\', \'" + user.login + "\', \'" +
+                user.password + "\', \'" + user.email + "\', \'" + user.status + "\');");
         },
         this.updateUser = function(user){
-            return db.query("UPDATE  \"users\" SET  \"full_name\"=\'" + user.fullName + "\', \"login\"= \'" + user.login + "\',\"password\"= \'" + user.password + "\', \"email\" = \'" + user.email + "\',\"status\"= \'" + user.status + "\' WHERE \"id\" = " + user.id + ";");
+            return db.query("UPDATE \"users\" SET \"full_name\" = \'" + user.fullName +
+                "\', \"login\" = \'" + user.login + "\', \"password\" = \'" + user.password +
+                "\', \"email\" = \'" + user.email + "\', \"status\" = \'" + user.status +
+                "\' WHERE \"id\" = " + user.id + ";");
         },
         this.deleteUser = function (id) {
             return db.query("DELETE FROM \"users\" WHERE \"id\" = " + id + ";");
