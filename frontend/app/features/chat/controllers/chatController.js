25e6301aa6077f209b49bf70e12ed8910af53fd9
Anna Slichenko <anslichenko@gmail.com>
2016-10-03 17:49:27 +0300

    add flash message

--- frontend/app/features/chat/controllers/chatController.js
+++ frontend/app/features/chat/controllers/chatController.js
@@ -1,7 +1,7 @@
 (function() {
     angular.module("em.chat").controller("em.chat.chatController", chatController);
 
-    function chatController($scope, chatService, $timeout) {
+    function chatController($scope, chatService, $timeout, FlashService, $rootScope) {
         var self = this;
         self.scrollBottom = function() {
             var objDiv = document.querySelector('.chat-body');
@@ -9,23 +9,25 @@
         };
         $scope.msgSend = function() {
             var msg = document.forms.publish.message.value;
+            FlashService.clearFlashMessage();
             if (msg !== '') {
                 chatService.msgSend(msg);
                 document.forms.publish.message.value = '';
                 $timeout(self.scrollBottom, 10);
             } else {
-                alert('Enter something!');
+              console.log('flaaaaash');
+                FlashService.Error('Please, enter something', false);
             }
         };
         // history
         $scope.history = chatService.history;
         // new messages
         $scope.live = chatService.live;
-
+        //scroll to bottom for new user
         $timeout(self.scrollBottom, 1000);
 
     }
 
-    chatController.$inject = ["$scope", "em.chat.chatService", "$timeout"];
+    chatController.$inject = ["$scope", "em.chat.chatService", "$timeout", "FlashService", "$rootScope"];
 
 })();
