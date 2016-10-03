caa96fa62a6b1de4df3b6ab96db5285b58be93a5
Anna Slichenko <anslichenko@gmail.com>
2016-10-03 16:08:51 +0300

    add word-break

--- frontend/app/features/chat/controllers/chatController.js
+++ frontend/app/features/chat/controllers/chatController.js
@@ -1,21 +1,20 @@
-
 (function() {
     angular.module("em.chat").controller("em.chat.chatController", chatController);
 
     function chatController($scope, chatService, $timeout) {
-       var self = this;
+        var self = this;
         self.scrollBottom = function() {
-          var objDiv = document.querySelector('.chat-body');
-          objDiv.scrollTop = objDiv.scrollHeight;
+            var objDiv = document.querySelector('.chat-body');
+            objDiv.scrollTop = objDiv.scrollHeight;
         };
         $scope.msgSend = function() {
             var msg = document.forms.publish.message.value;
-            if (msg !== ''){
-              chatService.msgSend(msg);
-              document.forms.publish.message.value = '';
-              $timeout(self.scrollBottom, 10);
+            if (msg !== '') {
+                chatService.msgSend(msg);
+                document.forms.publish.message.value = '';
+                $timeout(self.scrollBottom, 10);
             } else {
-              alert('Enter something!');
+                alert('Enter something!');
             }
         };
         // history
