34771492906e6173c0dd1d89690067dcb6709bf7
maryna.yelakova <maryna.yelakova@gmail.com>
2016-10-04 23:35:43 +0300

    move getSortClass to result-table view

--- frontend/app/features/result-table/controllers/result-table-controller.js
+++ frontend/app/features/result-table/controllers/result-table-controller.js
@@ -10,28 +10,15 @@
         $scope.sortColoumn = "name";
         $scope.reverseSort = false;
         
-        $scope.sortData = function(coloumn){
-            if ($scope.sortColoumn == coloumn){
+        $scope.sortData = function(coloumnName){
+            if ($scope.sortColoumn == coloumnName){
                 $scope.reverseSort = !$scope.reverseSort;
             }else{
                 $scope.reverseSort = false;
             }
-            $scope.sortColoumn = coloumn;
+            $scope.sortColoumn = coloumnName;
             return true;
         };
-
-        $scope.getSortClass = function(coloumn){
-            if ($scope.sortColoumn == coloumn){
-                if($scope.reverseSort){
-                    return "glyphicon glyphicon-arrow-up static";
-                }else{
-                    return "glyphicon glyphicon-arrow-down static";
-                }
-            }else{
-                return '';
-            }
-        
-        }
     }
     chessResultController.$inject = ["$scope", "em.result-table.result-table-service"]
 
