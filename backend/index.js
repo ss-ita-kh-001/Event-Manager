a3b146cbec11addce2f6efa5f70ce7eb87e37bdd
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-10 01:25:04 +0300

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 var bodyParser = require('body-parser');
 var router =  require('./router');
 var chat = require("./features/chat.js");
@@ -14,10 +13,6 @@ app.use(express.static(__dirname + '/../frontend'));
 
 app.listen(process.env.PORT || 5000, function(req, res) {
     console.log('Example app listening on port 5000!');
-});
-
-app.get('*', function(req, res) {
-    res.sendFile(path.resolve('frontend/app/index.html'));
 });
 
 router.init(app);
