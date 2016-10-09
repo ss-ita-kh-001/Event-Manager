a2f9a1532665ea36a38684ae4114c3dde7a37ed1
Anna Slichenko <anslichenko@gmail.com>
2016-10-09 11:39:02 +0300

    make login ctrl more readable

--- frontend/app/features/login/controllers/loginController.js
+++ frontend/app/features/login/controllers/loginController.js
@@ -1,35 +1,30 @@
-(function(){
-    'use strict';
+(function() {
 
-    angular.module('em.login').controller('em.login.loginController',loginController);
+    angular.module('em.login').controller('em.login.loginController', loginController);
 
     function loginController($scope, $location, authenticationService, flashService) {
 
-      $scope.login = login;
-      $scope.reset = reset;
-
-      function login() {
-          $scope.dataLoading = true;
-          authenticationService.logIn($scope.user.email, $scope.user.password, function (response) {
-              if (response.success) {
-                  authenticationService.setCredentials($scope.user.email, $scope.user.password);
-                  var users = JSON.parse(localStorage.users);
-                  var id;
-                  for (var i = 0; i < users.length; i++) {
-                    if (users[i].email == $scope.user.email) {
-                      id = users[i].id;
+        $scope.login = function() {
+            $scope.dataLoading = true;
+            authenticationService.logIn($scope.user.email, $scope.user.password, function(response) {
+                if (response.success) {
+                    authenticationService.setCredentials($scope.user.email, $scope.user.password);
+                    var users = JSON.parse(localStorage.users);
+                    var id;
+                    for (var i = 0; i < users.length; i++) {
+                        if (users[i].email == $scope.user.email) {
+                            id = users[i].id;
+                        }
                     }
-                  }
-                  $location.path('/profile/' + id);
-              } else {
-                  flashService.error(response.message);
-                  $scope.dataLoading = false;
-              }
-          });
-      };
-      function reset() {
-        authenticationService.clearCredentials();
-      };
-  }
-
+                    $location.path('/profile/' + id);
+                } else {
+                    flashService.error(response.message);
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+        $scope.reset = function() {
+            authenticationService.clearCredentials();
+        };
+    }
 })();
