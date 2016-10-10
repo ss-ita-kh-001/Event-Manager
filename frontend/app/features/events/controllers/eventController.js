0b2221b1e73e86bf49dbc5abf91afb6cedeb3c07
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-10 18:29:29 +0300

    remove localStorage service

--- frontend/app/features/events/controllers/eventController.js
+++ frontend/app/features/events/controllers/eventController.js
@@ -1,18 +1,18 @@
 (function() {
     angular.module("em.events").controller("em.events.eventController", eventController);
 
-    function eventController($scope, $routeParams, localStorageService) {
+    function eventController($scope, $routeParams) {
 
-          $scope.path = $routeParams.id;
-          $scope.events = JSON.parse(localStorageService.get('events'))
-          $scope.eventNotFound = false;
+        $scope.path = $routeParams.id;
+        $scope.events = JSON.parse(localStorageService.get('events'))
+        $scope.eventNotFound = false;
 
-          angular.forEach($scope.events, function(event,  path ){
-           if(event.id == $scope.path){
-             $scope.event = event;
-           }
-         });
+        angular.forEach($scope.events, function(event, path) {
+            if (event.id == $scope.path) {
+                $scope.event = event;
+            }
+        });
 
     }
-    eventController.$inject = ["$scope","$routeParams", "localStorageService"]
+    eventController.$inject = ["$scope", "$routeParams"]
 })();
