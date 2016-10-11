d499ce5cbcce0bb97c55aaa5378c177eedab8aa8
Anton Myshenin <myshenin.contact@gmail.com>
2016-10-11 16:57:34 +0300

    sql fixes + event api changes
    ATTENTION: event api changed. Look at router.js

--- backend/features/database/events.js
+++ backend/features/database/events.js
@@ -1,27 +1,26 @@
-    var db = require("./connection");
-    var events = function() {
-        this.getAll = function() {
-            return db.query("SELECT * FROM \"events\";");
-        };
-        this.getByEvent = function(id) {
-            return db.query("SELECT * FROM events WHERE id = '" + id + "';");
-        };
-        this.updateEvent = function(event) {
-            return db.query("UPDATE events SET title = '" + event.title +
-                "', \"desc\" = '" + event.desc + "', date = '" + event.date +
-                "', place = '" + event.place + "', report = '" + event.report +
-                "' WHERE id =  '" + event.id + "';");
-        };
-        this.addEvent = function(event) {
-            return db.query("INSERT INTO events( title, \"desc\", date, place, report)" +
-                " VALUES(\'" + event.title + "\', \'" + event.desc + "\', \'" +
-                event.date + "\', \'" + event.place + "\', \'" + event.report + "\');");
-        };
-        this.deleteEventById = function(id) {
-            return db.query("DELETE FROM events  WHERE id  = '" + id + "';");
-        };
-        this.getLastId = function() {
-            return db.query("SELECT \"id\" FROM \"events\" ORDER BY \"id\" DESC LIMIT 1;");
-        };
+var db = require("./connection");
+var events = function() {
+    this.getAll = function() {
+        return db.query("SELECT * FROM \"events\";");
     };
-    module.exports = events;
+    this.getByEvent = function(id) {
+        return db.query("SELECT * FROM \"events\" WHERE \"id\" = " + id + ";");
+    };
+    this.updateEvent = function(event) {
+        return db.query("UPDATE \"events\" SET \"title\" = \'" + event.title +
+            "\', \"desc\" = \'" + event.desc + "\', \"date\" = \'" + event.date +
+            "\', \"place\" = \'" + event.place + "\' WHERE \"id\" = " + event.id + ";");
+    };
+    this.addEvent = function(event) {
+        return db.query("INSERT INTO \"events\"(\"title\", \"desc\", \"date\", \"place\", \"photos\") " +
+            "VALUES(\'" + event.title + "\', \'" + event.desc + "\', \'" + event.date +
+            "\', \'" + event.place + "\', \'" + Math.ceil(Math.random() * Math.pow(10, 12)) + "\');");
+    };
+    this.deleteEventById = function(id) {
+        return db.query("DELETE FROM \"events\"  WHERE \"id\"  = " + id + ";");
+    };
+    this.getLastId = function() {
+        return db.query("SELECT \"id\" FROM \"events\" ORDER BY \"id\" DESC LIMIT 1;");
+    };
+};
+module.exports = events;
