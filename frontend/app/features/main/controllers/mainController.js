1259249adecfda4dcd986bcfd97ebee81b72bd72
ponata <pobernatali@gmail.com>
2016-10-13 16:10:50 +0300

    set up redirect from event-news to event's page, added nextNews and latestNews in main-news page

--- frontend/app/features/main/controllers/mainController.js
+++ frontend/app/features/main/controllers/mainController.js
@@ -3,74 +3,32 @@
 
     angular.module('em.main').controller('em.main.mainController', mainController);
 
-    function mainController($scope, eventNews, mainApiService) {
+    function mainController($scope, $location, eventNews, mainApiService) {
 
-        $scope.PickEventListNews = function () {
-            eventNews.getEventNews().then(function (response) {
-                $scope.pastEvents = findThreePastEvents(response.data);
-                $scope.upcomingEvents = findThreeUpcomingEvents(response.data); 
+        $scope.getEventListNews = function () {
+
+            eventNews.getNextEvents().then(function (response) {
+                $scope.nextEvents = response.data;
+            }, rejected);
+
+            eventNews.getLatestEvents().then(function (response) {
+                $scope.latestEvents = response.data;
             }, rejected);
          
         };
 
-        $scope.PickEventListNews();
+        $scope.getEventListNews();
 
         function rejected (error) {
             console.log('Error: ' + error.data.status);
         }
+
+        //redirect to event's page
+        $scope.eventPage = function (eventId) {
+           $location.path("/events/" + eventId);
+        };
+
     }
 
-
-// sort to show three past events
-    function findThreePastEvents (eventArray) {
-        var pastEventsArray = [];
-        var today = new Date();
-        eventArray = eventArray.sort(function (a, b) {
-            if (a.date > b.date) return 1;
-            if (a.date < b.date) return -1;
-            return 0;
-        });
-
-
-        for (var i=0; i<eventArray.length; i++) {
-            var eventDay = new Date(eventArray[i].date);
-            var isToday = today.getDate()  === eventDay.getDate() && today.getMonth()  === eventDay.getMonth() && today.getFullYear()  === eventDay.getFullYear();
-            var isLaterThanToday = eventDay > today;
-            if (isToday || isLaterThanToday) {
-                pastEventsArray.push(eventArray[i-1]);
-                pastEventsArray.push(eventArray[i-2]);
-                pastEventsArray.push(eventArray[i-3]);
-                break;
-            }
-        }
-
-        return pastEventsArray;
-    }
-
-// sort to show three upcoming events
-    function findThreeUpcomingEvents (eventArray) {
-        var upcomingEventsArray = [];
-        var today = new Date();
-        eventArray = eventArray.sort(function (a, b) {
-            if (a.date > b.date) return 1;
-            if (a.date < b.date) return -1;
-            return 0;
-        });
- 
-        for (var i=0; i<eventArray.length; i++) {
-            var eventDay = new Date(eventArray[i].date);
-            var isToday = today.getDate()  === eventDay.getDate() && today.getMonth()  === eventDay.getMonth() && today.getFullYear()  === eventDay.getFullYear();
-            var isLaterThanToday = eventDay > today;
-            if (isToday || isLaterThanToday) {
-                upcomingEventsArray.push(eventArray[i]);
-                upcomingEventsArray.push(eventArray[i+1]);
-                upcomingEventsArray.push(eventArray[i+2]);
-                break;
-            }
-        }
-
-        return upcomingEventsArray;
-    }
-
-    mainController.$inject = ["$scope", "em.main.event.service.news", "em.mainApiService"];
+    mainController.$inject = ["$scope", "$location", "em.main.event.service.news", "em.mainApiService"];
 })();
