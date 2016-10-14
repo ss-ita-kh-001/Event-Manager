153501bd1d8e6d2516e6726fdd53d93bbb5d7b69
Anna Slichenko <anslichenko@gmail.com>
2016-10-14 15:20:08 +0300

    add basic settings

--- backend/router.js
+++ backend/router.js
@@ -14,7 +14,9 @@
  */
   function ensureAuthenticated(req, res, next) {
       if (!req.header('Authorization')) {
-          return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+          return res.status(401).send({
+              message: 'Please make sure your request has an Authorization header'
+          });
       }
       var token = req.header('Authorization').split(' ')[1];
 
@@ -22,11 +24,15 @@
       try {
           payload = jwt.decode(token, config.TOKEN_SECRET);
       } catch (err) {
-          return res.status(401).send({ message: err.message });
+          return res.status(401).send({
+              message: err.message
+          });
       }
 
       if (payload.exp <= moment().unix()) {
-          return res.status(401).send({ message: 'Token has expired' });
+          return res.status(401).send({
+              message: 'Token has expired'
+          });
       }
       req.user = payload.sub;
       next();
@@ -45,6 +51,66 @@
               }).catch(function(error) {
                   res.status(500).send(error);
                   console.log(error);
+              });
+          });
+          app.get("/forgot", function(req, res) {
+              res.render("forgot", {
+                  user: req.user
+              });
+          });
+          app.post("/forgot", function(req, res, next) {
+              async.waterfall([
+                  function(done) {
+                      crypto.randomBytes(20, function(err, buf) {
+                          var token = buf.toString('hex');
+                          done(err, token);
+                      });
+                  },
+                  function(token, done) {
+                    users.getUserByEmail(req.body.email).then(function() {
+                      var resetPasswordToken = token;
+                      var resetPasswordExpires = Date.now() + 3600000; // 1 hour
+                    })
+                  /*    User.findOne({
+                          email: req.body.email
+                      }, function(err, user) {
+                          if (!user) {
+                              req.flash('error', 'No account with that email address exists.');
+                              return res.redirect('/forgot');
+                          }*/
+
+                    //      user.save(function(err) {
+                      //        done(err, token, user);
+                    //      });
+
+                  },
+                  function(token, user, done) {
+                      var smtpTransport = nodemailer.createTransport('SMTP', {
+                          service: 'gmail',
+                          auth: {
+                            user: 'event.manager.notification@gmail.com',
+                            pass: 'ss-ita-kh-001'
+                          }
+
+                      });
+                      var mailOptions = {
+                          to: user.email,
+                          from: 'event.manager.notification@gmail.com',
+                          subject: 'Node.js Password Reset',
+                          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                              'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                              'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                              'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+                      };
+                      smtpTransport.sendMail(mailOptions, function(err) {
+                          console.log('email sent');
+                          //req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                          done(err, 'done');
+                      });
+                  }
+              ], function(err) {
+                  if (err) return next(err);
+                  res.redirect('/forgot');
               });
           });
           app.get(apiPreff + "/users", function(req, res) {
