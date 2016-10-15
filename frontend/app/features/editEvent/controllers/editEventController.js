9a58eadfbbc4ab9630afe1e9b4bb121276c06757
Daria <liadonne1988@gmail.com>
2016-10-15 22:04:00 +0300

    add datePicker directive

--- frontend/app/features/editEvent/controllers/editEventController.js
+++ frontend/app/features/editEvent/controllers/editEventController.js
@@ -1,37 +1,38 @@
 (function() {
     angular.module("em.editEvent").controller("em.editEvent.editEventController", editEventController);
 
-    function editEventController($scope, $rootScope, $routeParams, datePicker, eventService) {
+    function editEventController($scope, $rootScope, $routeParams, eventService) {
 
-      $scope.datePicker = datePicker;
-      $scope.id = $routeParams.id;
+
+        $scope.id = $routeParams.id;
 
         $scope.getEventPromise = eventService.getEvent($scope.id);
-        $scope.getEventPromise.then(function (response) {
-              $scope.event = response.data[0];
-             $scope.search()
-          }, rejected );
+        $scope.getEventPromise.then(function(response) {
+            $scope.event = response.data[0];
+            $scope.event.date = new Date($scope.event.date);
+            $scope.search()
+        }, rejected);
 
-    //  console.log('scope',$scope.event.place)
+        //  console.log('scope',$scope.event.place)
 
-      function rejected (error) {
-          console.log('Error: ' + error.data.status);
-      }
+        function rejected(error) {
+            console.log('Error: ' + error.data.status);
+        }
 
         $scope.search = function() {
-                $rootScope.search($scope.event.place)
-                    .then(function(res) { // success
-                            $rootScope.addMarker(res);
-                        },
-                        function(status) { // error
-                            $scope.apiError = true;
-                            $scope.apiStatus = status;
-                        }
-                    );
-            }
+            $rootScope.search($scope.event.place)
+                .then(function(res) { // success
+                        $rootScope.addMarker(res);
+                    },
+                    function(status) { // error
+                        $scope.apiError = true;
+                        $scope.apiStatus = status;
+                    }
+                );
+        }
 
 
-      $scope.update = function() {
+        $scope.update = function() {
             console.log($scope.event)
             eventService.update(Object.assign({}, $scope.event));
         }
@@ -39,6 +40,6 @@
 
     }
 
-    editEventController.$inject = ["$scope", "$rootScope", "$routeParams", "em.editEvent.datePicker", "em.events.eventService"];
+    editEventController.$inject = ["$scope", "$rootScope", "$routeParams", "em.events.eventService"];
 
 })();
