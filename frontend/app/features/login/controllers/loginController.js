4a2715cd44bf2c63f27a9049e32eee48db3dd545
Anna Slichenko <anslichenko@gmail.com>
2016-10-17 23:07:30 +0300

    remove DOM elements and fix class setting

--- frontend/app/features/login/controllers/loginController.js
+++ frontend/app/features/login/controllers/loginController.js
@@ -15,7 +15,7 @@
                     localStorage.setItem('fullName', res.data.user.full_name);
                     $scope.id = localStorage.getItem('userId');
                     $location.path('/profile/' + $scope.id);
-
+                    $scope.classHandler();
                     var getUserEventsPromise = userService.getUserEvents($scope.id);
                     getUserEventsPromise.then(function(res) {
                         angular.forEach(res.data, function(value, key) {
