c514edbf5dc0122e93cf001858bf130442d33ff8
Anna Slichenko <anslichenko@gmail.com>
2016-10-27 20:21:33 +0300

    clean up controllers - remove sorting functions

--- spec/test-event-list-spec.js
+++ spec/test-event-list-spec.js
@@ -13,37 +13,6 @@ describe("Controller Test", function () {
         });
     }));
 
-    it("Default value reverseSort", function () {
-        expect(mockScope.reverseSort).toBeFalsy();
-    })
-    it("Default value sortColumn", function () {
-        expect(mockScope.sortColumn).toEqual("title");
-    })
-    describe("Function sortData: sort column is not changed", function() {
-        beforeEach(function() {
-            mockScope.sortData("title");
-        });
-
-        it("New value reverseSort", function () {
-            expect(mockScope.reverseSort).toBeTruthy();
-        });
-        it("Value sortColumn", function () {
-            expect(mockScope.sortColumn).toEqual("title");
-        });
-    });
-    describe("Function sortData: sort column is changed", function() {
-        beforeEach(function() {
-            mockScope.sortData("date");
-        });
-
-        it("Value reverseSort", function () {
-            expect(mockScope.reverseSort).toBeFalsy();
-        });
-        it("Value sortColumn", function () {
-            expect(mockScope.sortColumn).toEqual("date");
-        });
-    });
-
     describe("HTTP service test", function () {
 
         var backend;
