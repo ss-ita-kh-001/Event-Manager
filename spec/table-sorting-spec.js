93b06255318eefc6d0b1a50f85dbc63e412b58a0
Anna Slichenko <anslichenko@gmail.com>
2016-10-27 20:13:10 +0300

    clean up table-sorting spec

--- spec/table-sorting-spec.js
+++ spec/table-sorting-spec.js
@@ -1,8 +1,6 @@
 describe('table sorting directive', function() {
     var $compile,
         element,
-        scope,
-        attrs,
         $rootScope;
 
     beforeEach(angular.mock.module("em"));
@@ -10,29 +8,26 @@ describe('table sorting directive', function() {
     beforeEach(inject(function(_$compile_, _$rootScope_) {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
+      element = $compile("<th class='col-sm-2 cursor-pointer' table-sorting='title'></th>")($rootScope);
     }));
 
     it('check for initil values in reverseSort and predicate', function() {
-      var element = $compile("<th class='col-sm-2 cursor-pointer' table-sorting='title'></th>")($rootScope);
       expect($rootScope.predicate).toEqual("title");
       expect($rootScope.reverseSort).toBeFalsy();
     });
 
     it("check for a new value in reverseSort on click", function () {
-        var element = $compile("<th class='col-sm-2 cursor-pointer' table-sorting='title'></th>")($rootScope);
         element.triggerHandler('click');
         expect($rootScope.reverseSort).toBeTruthy();
     });
 
     it("check for a new value in reverseSort on 2 clicks", function () {
-        var element = $compile("<th class='col-sm-2 cursor-pointer' table-sorting='title'></th>")($rootScope);
         element.triggerHandler('click');
         element.triggerHandler('click');
         expect($rootScope.reverseSort).toBeFalsy();
     });
 
     it("check for a new value in reverseSort on 3 clicks", function () {
-        var element = $compile("<th class='col-sm-2 cursor-pointer' table-sorting='title'></th>")($rootScope);
         element.triggerHandler('click');
         element.triggerHandler('click');
         element.triggerHandler('click');
