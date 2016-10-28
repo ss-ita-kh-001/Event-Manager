e593ef2f7fb34c5c86c29f57ffe90367f5ea3191
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-28 19:20:44 +0300

    select only necessary columns for chat from db

--- backend/features/database/chat.js
+++ backend/features/database/chat.js
@@ -1,12 +1,15 @@
 var db = require("./connection");
 var chat = function() {
     this.getHistory = function() {
-        return db.query("SELECT * FROM \"chat\" INNER JOIN \"users\" ON \"chat\".\"user\" = \"users\".\"id\" ;");
+        return db.query("SELECT \"full_name\",\"user\",\"date\",\"text\"  FROM \"chat\" INNER JOIN \"users\" ON \"chat\".\"user\" = \"users\".\"id\" ;");
     };
     this.addMessage = function(user) {
         // console.log(user);
         return db.query("INSERT INTO \"chat\"(\"user\",\"date\", \"text\") " + "VALUES(\'" + user.decodedId + "\', \'" + user.date + "\', \'" + user.text + "\');");
     };
+    this.getMessage = function (user) {
+        return db.query("SELECT * FROM \"chat\" INNER JOIN \"users\" ON \"chat\".\"user\" = \"users\".\"id\" ;");
+    }
     this.getLastId = function() {
         return db.query("SELECT \"id\" FROM \"chat\" ORDER BY \"id\" DESC LIMIT 1;");
     };
