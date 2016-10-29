9f789ee3355ff39ac6465bb7170280f3ffd939cc
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-30 00:37:37 +0300

    add only 'latin symbols' rule

--- frontend/app/features/register/controllers/registerController.js
+++ frontend/app/features/register/controllers/registerController.js
@@ -5,11 +5,12 @@
 
     function registerController($scope, userService, flashService) {
         $scope.range = '.{6,16}';
-        $scope.username = /^[^"?!@#$%^&*()>_<,.\\/|{}\[\];=`~+\-\d]{3,50}$/;
+        $scope.username = /^[^"?!@#$%^&*()>_<,.\\/|{}\[\];=`~+\-\d][a-zA-Z]{3,50}$/;
         $scope.register = function() {
             $scope.dataLoading = true;
             userService.create($scope.user);
         };
+
         $scope.checkPass = function() {
             if ($scope.signupForm.password.$invalid) {
                 flashService.error('The password must be between 6 and 16 characters long', false);
