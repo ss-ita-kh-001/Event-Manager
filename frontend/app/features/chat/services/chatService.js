b382cd4330a7b259bda8abe22b43aff2c892d734
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-29 13:02:34 +0300

    Remove unused dependencies in chat service

--- frontend/app/features/chat/services/chatService.js
+++ frontend/app/features/chat/services/chatService.js
@@ -1,7 +1,7 @@
 (function() {
-    angular.module("em.chat").service("em.chat.chatService", ["$rootScope", "$location", "flashService", chatService]);
+    angular.module("em.chat").service("em.chat.chatService", ["$rootScope", "flashService", chatService]);
 
-    function chatService($rootScope, $location, flashService) {
+    function chatService($rootScope, flashService) {
 
         var host = location.origin.replace(/^http/, 'ws');
         var socket = new WebSocket(host);
