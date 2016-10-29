4c08d4e50b1ef183cf7946dd18f35b6afbe6d676
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-29 21:17:57 +0300

    fix

--- frontend/app/features/chat/services/chatService.js
+++ frontend/app/features/chat/services/chatService.js
@@ -29,6 +29,8 @@
             console.log(response);
             if (!response.error) {
                 angular.forEach(response.data, function(value, key) {
+                    // console.log(response.data[key].date);
+                    response.data[key].date = response.data[key].date.substring(11, 19);
                 });
                 // if single msg
                 if (response.data.length == 1) {
