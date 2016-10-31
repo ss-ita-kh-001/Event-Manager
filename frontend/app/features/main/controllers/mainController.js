f105e12dbaabdc335b6f737f6ee5738c4cc8a6e7
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-10-31 23:59:30 +0200

    first merge

--- frontend/app/features/main/controllers/mainController.js
+++ frontend/app/features/main/controllers/mainController.js
@@ -10,17 +10,12 @@
             eventNews.getNextEvents().then(function (response) {
                 $scope.nextEvents = response.data;
                 $scope.descriptionLimit = 130;
-                //TODO: remove this mock-data after setting field-src in database
-                $scope.nextEvents[0].src = './build/img/football.jpg';
-                $scope.nextEvents[1].src = './build/img/chess.jpg';
-                $scope.nextEvents[2].src = './build/img/picnic.jpg';
             }, rejected);
 
             eventNews.getLatestEvents().then(function (response) {
                 $scope.latestEvents = response.data;
                 $scope.letterLimit = 100;
             }, rejected);
-
         };
 
         $scope.getEventListNews();
