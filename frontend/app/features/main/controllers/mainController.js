48eb0fa01b538a4da50b0f321bab691c5aff371c
Anton Myshenin <myshenin.contact@gmail.com>
2016-10-31 20:44:29 +0200

    fixes to description in event list

--- frontend/app/features/main/controllers/mainController.js
+++ frontend/app/features/main/controllers/mainController.js
@@ -9,11 +9,17 @@
 
             eventNews.getNextEvents().then(function (response) {
                 $scope.nextEvents = response.data;
+                for(var i = 0; i < $scope.nextEvents.length; i++){
+                  $scope.nextEvents[i].desc = $scope.nextEvents[i].desc.replace(/(<([^>]+)>)/g, "").substring(0, 100) + "...";
+                }
                 $scope.descriptionLimit = 130;
             }, rejected);
 
             eventNews.getLatestEvents().then(function (response) {
                 $scope.latestEvents = response.data;
+                for(var i = 0; i < $scope.latestEvents.length; i++){
+                  $scope.latestEvents[i].desc = $scope.latestEvents[i].desc.replace(/(<([^>]+)>)/g, "").substring(0, 100) + "...";
+                }
                 $scope.letterLimit = 100;
             }, rejected);
         };
