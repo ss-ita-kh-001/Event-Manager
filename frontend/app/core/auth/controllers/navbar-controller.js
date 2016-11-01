92e93ace3119922c521a2c10fa051d668ab4137a
Anna Slichenko <anslichenko@gmail.com>
2016-11-01 20:47:10 +0200

    add test commit

--- frontend/app/core/auth/controllers/navbar-controller.js
+++ frontend/app/core/auth/controllers/navbar-controller.js
@@ -2,7 +2,7 @@
 
     angular.module('em').controller('em.navbarController', navbarController);
 
-    function navbarController($scope, $auth, $location, $timeout) {
+    function navbarController($scope, $auth, $location, $timeout, userService) {
         $scope.isAuthenticated = function() {
             return $auth.isAuthenticated();
         };
@@ -31,10 +31,28 @@
             href: '/profile/' + localStorage.getItem("userId"),
             name: 'profile'
         }];
+        $scope.isAdmin = userService.isAdmin();
+
+    /*    $scope.getCurrentUser = function() {
+            if (userService.getUserInfo()) {
+                $scope.currentUser = userService.getUserInfo();
+                return;
+            }
+            if (localStorage.getItem("userId")) {
+                userService.getById(localStorage.getItem("userId"))
+                    .then(function(response) {
+                        if (Array.isArray(response) && response.length > 0) {
+                            userService.setUserInfo(response[0]);
+                            $scope.currentUser = userService.getUserInfo();
+                        }
+                    });
+            };
+        };*/
         $scope.idInit = function() {
             $scope.menuItems[6].href = '/profile/' + localStorage.getItem("userId");
         };
         $scope.setActiveClass = function() {
+            $scope.isAdmin = userService.isAdmin();
             var path = $location.path();
             $scope.idInit();
             for (var i = 0; i < $scope.menuItems.length; i++) {
@@ -46,6 +64,8 @@
         $scope.classHandler = function() {
             $timeout($scope.setActiveClass, 10);
         };
+      //  $scope.getCurrentUser();
         $scope.setActiveClass();
     }
+    navbarController.$inject = ['$scope', '$auth', '$location', '$timeout', 'userService'];
 })();
