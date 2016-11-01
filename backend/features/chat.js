913e20033e9c83ddc94ee527e1f856c3c945551d
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-11-01 19:52:20 +0200

    create function sendError on server chat

--- backend/features/chat.js
+++ backend/features/chat.js
@@ -23,14 +23,12 @@ var chat = {
 
         webSocketServer.on('connection', function(socket) {
             var id = Math.random();
-            console.log('connected', id);
             clients[id] = socket;
 
             var currentIndex;
 
             socket.on('message', function(obj) {
                 console.log('-----------------------------------------------------');
-
                 // if token can be decoded and isn't expired
                 var requestExt = isAuth(obj);
                 // console.log(requestExt);
@@ -39,7 +37,6 @@ var chat = {
                     // console.log(requestExt);
                     if (!requestExt.data.getHistory) {
                         console.log('single message');
-                        // console.log('requestExt', requestExt);
                         requestExt.data.text = validate.makeTrusted(requestExt.data.text);
                         chatDb.addMessage(requestExt.data).then(function(res) {
                             chatDb.getMessage().then(function(data, res) {
@@ -49,64 +46,57 @@ var chat = {
                                 }
                             }).catch(function(error) {
                                 console.log(error);
-                                requestExt.error = true;
-                                requestExt.errorMessage = error;
-                                clients[id].send(JSON.stringify(requestExt));
+                                sendError(clients, id, requestExt, error);
                             });
 
                         }).catch(function(error) {
                             console.log(error);
-                            requestExt.error = true;
-                            requestExt.errorMessage = error;
-                            clients[id].send(JSON.stringify(requestExt));
+                            sendError(clients, id, requestExt, error);
                         });
                     } else {
-                        // console.log('index from client', requestExt.data.index);
                         // if index is defined on client
-
                         requestExt.data.getHistory = false;
                         chatDb.getLastId().then(function(data, res) {
                             currentIndex = data[0].id;
                             console.log('get Last id', currentIndex);
-
                             if (requestExt.data.index) {
-                                // console.log('requestExt.data.index true', requestExt.data.index);
+                                console.log('requestExt.data.index true', requestExt.data.index);
                                 currentIndex = requestExt.data.index;
                             }
                             console.log('index from client', requestExt.data.index);
-
                             chatDb.getHistory(currentIndex).then(function(data, res) {
                                 // console.log('getHistory with currentIndex', currentIndex);
                                 response.data = data;
                                 response.index = (currentIndex - 10);
-                                console.log('send index to client', id, response.index);
                                 clients[id].send(JSON.stringify(response));
                             }).catch(function(error) {
-                                response.error = true;
-                                response.errorMessage = error;
+                                sendError(clients, id, requestExt, error);
                             });
                         }).catch(function(error) {
-                            response.error = true;
-                            response.errorMessage = error;
+                            sendError(clients, id, requestExt, error);
                         });
-
                     }
-
                 } else {
-                    requestExt.error = true;
-                    requestExt.errorMessage = 'Incorrect token. Try to login again';
-                    clients[id].send(JSON.stringify(requestExt));
+                    sendError(clients, id, requestExt, 'Incorrect token. Try to login again');
                 }
-
             });
 
             socket.on('close', function() {
                 console.log('disconnected', id);
                 delete clients[id];
             });
+
+
         });
+
     }
 };
+
+function sendError(clients, id, response, status) {
+    response.error = true;
+    response.errorMessage = status;
+    clients[id].send(JSON.stringify(response));
+}
 
 function isAuth(req) {
     var data = JSON.parse(req);
@@ -134,4 +124,4 @@ function isAuth(req) {
     return obj;
 }
 
-module.exports = chat;;
+module.exports = chat;
