6f1110bd9470c47852c3d0506beb19326409e55e
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-11-02 00:03:00 +0200

    Revert "refator"
    This reverts commit 32b36253c25786842f079d94872f2a13e4834261.

--- frontend/app/core/auth/services/user.service.js
+++ frontend/app/core/auth/services/user.service.js
@@ -6,6 +6,7 @@
     userService.$inject = ['em.mainApiService', '$location', 'flashService', '$rootScope'];
 
     function userService(mainApiService, $location, flashService, $rootScope) {
+        $rootScope.itemsPerPage = 10;
         $rootScope.allUsers = [];
 
         var service = {};
@@ -29,7 +30,6 @@
         return service;
 
         function getUsers(index) {
-            console.log('getUsers index', index);
             return mainApiService.get('users', index).then(handleSuccess, handleError('Error getting all users'));
         }
 
