4fe396c081a21d791114e850efb6caa0a5b17a59
Anton Myshenin <myshenin.contact@gmail.com>
2016-11-03 12:44:18 +0200

    service $1

--- spec/add-event-spec.js
+++ spec/add-event-spec.js
@@ -1,14 +1,26 @@
 describe("Add event", function() {
     var scope = {};
     var controller;
+    var mockAddEventService;
+    var mock$Route;
 
     beforeEach(angular.mock.module("em"));
     beforeEach(angular.mock.module(function($provide) {
-
+        $provide.service("em.addEvent.addEventService", function() {
+            this.addEvent = jasmine.createSpy("addEvent");
+            this.updateWithImage = jasmine.createSpy("updateWithImage");
+            this.update = jasmine.createSpy("update");
+        });
+        $provide.service("$route");
     }));
-    beforeEach(angular.mock.inject(function($controller) {
+    beforeEach(angular.mock.inject(function($controller, addEventService, $route) {
+        scope = $rootScope.$new();
+        mockAddEventService = addEventService;
+        mock$Route = $route;
         controller = $controller("em.addEvent.addEventController", {
-            $scope: scope
+            $scope: scope,
+            addEventService: mockAddEventService,
+            $route: mock$Route
         });
     }));
 
@@ -18,7 +30,7 @@ describe("Add event", function() {
             desc: "TEST EVENT: title",
             date: "2016-01-01",
             place: "London",
-            avatar: null;
+            avatar: null
         });
     });
 });
